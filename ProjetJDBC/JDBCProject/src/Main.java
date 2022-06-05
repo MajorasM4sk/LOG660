ca4ef5b82c9a061dab9a74f620c5c141c251f936
MajorasMask <jeremy.gauthier01@gmail.com>
2022-06-05 14:46:46 -0400

    feuille de route v2

--- ProjetJDBC/JDBCProject/src/Main.java
+++ ProjetJDBC/JDBCProject/src/Main.java
@@ -1,27 +1,49 @@
-import POJOs.Videotheque;
+import POJOs.*;
 
 import java.sql.*;
+import java.util.LinkedList;
+import java.util.List;
 
 public class Main {
     public static void main(String argv[]) {
         String url = "jdbc:oracle:thin:EQUIPE112/C66VmkzD@log660ora12c.logti.etsmtl.ca:1521:LOG660";
         try {
-            Connection conn = DriverManager.getConnection(url);
-            if (conn != null) {
+            Connection connection = DriverManager.getConnection(url);
+            if (connection != null) {
                 System.out.println("Connexion à la BD réussie !");
                 Videotheque videotheque = XmlParser.fetchData();
-                System.out.println("Début de l'insertion des clients, cartes de crédit, types de forfait, forfaits clients...");
-                System.out.println("Début de l'insertion des personnes...");
-                System.out.println("Insertion des films, roles des acteurs, réalisateurs des films...");
+                System.out.println("Insertion des clients...");
+                insertClients(connection, videotheque.clients);
+                System.out.println("Insertion des cartes de crédit...");
+                insertCartesCredit(connection, videotheque.clients);
+                System.out.println("Insertion des types de forfait...");
+                insertForfaits(connection);
+                System.out.println("Insertion des forfaits clients...");
+                insertForfaitsClients(connection, videotheque.clients);
+                System.out.println("Insertion des personnes...");
+                insertPersonnes(connection, videotheque.personnes);
+                System.out.println("Insertion des films...");
+                insertFilms(connection, videotheque.films);
+                System.out.println("Insertion des roles des acteurs...");
+                insertRoles(connection, videotheque.films);
+                System.out.println("Insertion des réalisateurs...");
+                insertRealisateurs(connection, videotheque.films);
                 System.out.println("Détection des genres...");
-                System.out.println("Insertion des genres, table de liaison film-genre...");
+                List<String> genres = listerGenres(videotheque.films);
+                System.out.println("Insertion des genres...");
+                insertGenres(connection, genres);
                 System.out.println("Insertion des liaisons film-genre...");
+                insertGenresFilms(connection, videotheque.films);
                 System.out.println("Détection des pays...");
-                System.out.println("Insertion des pays, table de liaison film-pays...");
+                List<String> pays = listerPays(videotheque.films);
+                System.out.println("Insertion des pays...");
+                insertPays(connection, pays);
                 System.out.println("Insertion des liaisons film-pays...");
-                System.out.println("Début de l'insertion d'employés bidons...");
-                System.out.println("Début de l'insertion des copies de film aléatoirement (1-100)...");
-
+                insertPaysFilms(connection, videotheque.films);
+                System.out.println("Insertion d'employés bidons...");
+                insertEmployes(connection);
+                System.out.println("Insertion des copies de film aléatoirement pour chaque film (1-100)...");
+                insertCopies(connection, videotheque.films);
                 //exemple de prepared statement avec batch
                 /*String sql = "INSERT INTO client (id_client, courriel, mot_de_passe, telephone, nom, prenom, date_naissance) VALUES (?, ?, ?, ?, ?, ?, ?)";
                 PreparedStatement statement = conn.prepareStatement(sql);
@@ -45,11 +67,75 @@ public class Main {
 
                 int[] count = statement.executeBatch();*/
 
-                conn.commit();
-                conn.close();
+                connection.commit();
+                connection.close();
             }
         } catch (SQLException e) {
             e.printStackTrace();
         }
+    }
+
+    private static void insertClients(Connection connection, List<Client> clients) {
+
+    }
+
+    private static void insertCartesCredit(Connection connection, List<Client> clients) {
+
+    }
+
+    private static void insertForfaits(Connection connection) {
+        //voir le pdf d'énoncé du tp pour les types de forfaits (le tableau en haut du document)
+    }
+
+    private static void insertForfaitsClients(Connection connection, List<Client> clients) {
+
+    }
+
+    private static void insertPersonnes(Connection connection, List<Personne> personnes) {
+
+    }
+
+    private static void insertFilms(Connection connection, List<Film> films) {
+
+    }
+
+    private static void insertRoles(Connection connection, List<Film> films) {
+
+    }
+
+    private static void insertRealisateurs(Connection connection, List<Film> films) {
+
+    }
+
+    private static List<String> listerGenres(List<Film> films) {
+        //faire une liste des genres possibles pour les films, dont les éléments doivent être uniques
+        return new LinkedList<String>();
+    }
+
+    private static void insertGenres(Connection connection, List<String> genres) {
+
+    }
+    private static void insertGenresFilms(Connection connection, List<Film> films) {
+
+    }
+
+    private static List<String> listerPays(List<Film> films) {
+        //faire une liste des pays possibles pour les films, dont les éléments doivent être uniques
+        return new LinkedList<String>();
+    }
+
+    private static void insertPays(Connection connection, List<String> pays) {
+
+    }
+    private static void insertPaysFilms(Connection connection, List<Film> films) {
+
+    }
+
+    private static void insertEmployes(Connection connection) {
+        //have fun
+    }
+
+    private static void insertCopies(Connection connection, List<Film> films) {
+        //nb aléatoire de 1 à 100
     }
 }
