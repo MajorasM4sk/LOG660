7855449193c0da35d1230d7edbb69b2261e830e0
MajorasMask <jeremy.gauthier01@gmail.com>
2022-06-05 13:47:06 -0400

    exemple addBatch

--- ProjetJDBC/JDBCProject/src/Main.java
+++ ProjetJDBC/JDBCProject/src/Main.java
@@ -1,5 +1,4 @@
-import java.sql.Connection;
-import java.sql.DriverManager;
+import java.sql.*;
 
 public class Main {
     public static void main(String argv[]) {
@@ -8,9 +7,33 @@ public class Main {
             Connection conn = DriverManager.getConnection(url);
             if (conn != null) {
                 System.out.println("Connecté");
+                String sql = "INSERT INTO client (id_client, courriel, mot_de_passe, telephone, nom, prenom, date_naissance) VALUES (?, ?, ?, ?, ?, ?, ?)";
+                PreparedStatement statement = conn.prepareStatement(sql);
+                conn.setAutoCommit(false);
+                statement.setInt(1, 1);
+                statement.setString(2, "test@gmail.com");
+                statement.setString(3, "12345");
+                statement.setString(4, "4444444419");
+                statement.setString(5, "Gertrude");
+                statement.setString(6, "Lapoule");
+                statement.setString(7, "2021-04-19");
+                statement.addBatch();
+                statement.setInt(1, 2);
+                statement.setString(2, "test2@gmail.com");
+                statement.setString(3, "123456");
+                statement.setString(4, "4444444418");
+                statement.setString(5, "Gertrud");
+                statement.setString(6, "Lapoul");
+                statement.setString(7, "2021-04-18");
+                statement.addBatch();
+
+                int[] count = statement.executeBatch();
+
+                conn.commit();
+                conn.close();
             }
-        } catch (Exception e) {
-            System.out.println("Pas connecté");
+        } catch (SQLException e) {
+            e.printStackTrace();
         }
     }
 }
